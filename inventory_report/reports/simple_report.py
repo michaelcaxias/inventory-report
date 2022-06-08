e670b61a24be4b05707a3b4e4a0d89ffc53383d9
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-08 19:03:48 -0300

    :sparkles: feat: implementa função para pegar a data de fabricação mais antiga

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,8 +1,11 @@
 class SimpleReport:
+    def get_oldest_fabrication_date(self, products):
+        return min([product["data_de_fabricacao"] for product in products])
+
     def generate(self, dict):
-        oldest_fabrication_date = ''
-        next_expiration_date = ''
-        company_with_more_products = ''
+        oldest_fabrication_date = self.get_oldest_fabrication_date(dict)
+        next_expiration_date = ""
+        company_with_more_products = ""
 
         return f""""
           Data de fabricação mais antiga: {oldest_fabrication_date}
