c3c3f9e3c2676192b735c240789bc1fb7ecca0c6
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-08 18:43:09 -0300

    :test_tube: test: implementa testes para método __repr__ em classe product

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from datetime import date
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    today = date.today()
+    product = Product(
+      id=1,
+      nome_da_empresa="Michael Produtos",
+      nome_do_produto="Ultrabook",
+      data_de_fabricacao=today,
+      data_de_validade="2030-06-08",
+      numero_de_serie="050507",
+      instrucoes_de_armazenamento="Abra e use"
+    )
+
+    expected_value = (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
+    assert product.__repr__() == expected_value
