fc6f3fad4eac15f1a07017f511ed8c1ea6089c59
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-13 18:51:24 -0300

    :sparkles: feat: utiliza Counter para contar companias com mais produtos

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from datetime import date
 
 
@@ -7,17 +8,22 @@ class SimpleReport:
 
     def get_next_expiration_date(self, products):
         filter_nexts_projects = filter(
-          lambda product: product["data_de_validade"] > str(date.today()),
-          products
+            lambda product: product["data_de_validade"] > str(date.today()),
+            products,
         )
         return min(
-          [product["data_de_validade"] for product in filter_nexts_projects]
+            [product["data_de_validade"] for product in filter_nexts_projects]
         )
+
+    def get_company_more_products(self, products):
+        return Counter(
+            [product["nome_da_empresa"] for product in products]
+        ).most_common(1)[0][0]
 
     def generate(self, products):
         oldest_fabrication_date = self.get_oldest_fabrication_date(products)
         next_expiration_date = self.get_next_expiration_date(products)
-        company_with_more_products = ""
+        company_with_more_products = self.get_company_more_products(products)
 
         return f""""
           Data de fabricação mais antiga: {oldest_fabrication_date}
@@ -27,16 +33,16 @@ class SimpleReport:
 
 
 test_produts = [
-     {
-       "id": 1,
-       "nome_do_produto": "CADEIRA",
-       "nome_da_empresa": "Forces of Nature",
-       "data_de_fabricacao": "2022-04-04",
-       "data_de_validade": "2023-02-09",
-       "numero_de_serie": "FR48",
-       "instrucoes_de_armazenamento": "Conservar em local fresco"
-     }
-   ]
+    {
+        "id": 1,
+        "nome_do_produto": "CADEIRA",
+        "nome_da_empresa": "Forces of Nature",
+        "data_de_fabricacao": "2022-04-04",
+        "data_de_validade": "2023-02-09",
+        "numero_de_serie": "FR48",
+        "instrucoes_de_armazenamento": "Conservar em local fresco",
+    }
+]
 
-simple_report = SimpleReport().get_next_expiration_date(test_produts)
+simple_report = SimpleReport().get_company_more_products(test_produts)
 print(simple_report)
