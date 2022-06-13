4f80f425d6e2b2538e99fec59174e03a69df76c9
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-13 19:16:08 -0300

    :sparkles: feat: configura classe corretamente

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,38 +2,40 @@ from collections import Counter
 from datetime import date
 
 
-def get_oldest_fabrication_date(products):
-    return min([product["data_de_fabricacao"] for product in products])
-
-
-def get_next_expiration_date(products):
-    filter_nexts_projects = filter(
-        lambda product: product["data_de_validade"] > str(date.today()),
-        products,
-    )
-    return min(
-        [product["data_de_validade"] for product in filter_nexts_projects]
-    )
-
-
-def get_company_more_products(products):
-    return Counter(
-        [product["nome_da_empresa"] for product in products]
-    ).most_common(1)[0][0]
 
 
 class SimpleReport:
+    def get_oldest_fabrication_date(self, products):
+        return min([product["data_de_fabricacao"] for product in products])
 
-    def generate(self, products):
-        oldest_fabrication_date = get_oldest_fabrication_date(products)
-        next_expiration_date = get_next_expiration_date(products)
-        company_with_more_products = get_company_more_products(products)
 
-        return f""""
-          Data de fabricação mais antiga: {oldest_fabrication_date}
-          Data de validade mais próxima: {next_expiration_date}
-          Empresa com mais produtos: {company_with_more_products}
-        """
+    def get_next_expiration_date(self, products):
+        filter_nexts_projects = filter(
+            lambda product: product["data_de_validade"] > str(date.today()),
+            products,
+        )
+        return min(
+            [product["data_de_validade"] for product in filter_nexts_projects]
+        )
+
+
+    def get_company_more_products(self, products):
+        return Counter(
+            [product["nome_da_empresa"] for product in products]
+        ).most_common(1)[0][0]
+
+    @classmethod
+    def generate(cls, products):
+        oldest_fabrication_date = cls.get_oldest_fabrication_date(cls, products)
+        next_expiration_date = cls.get_next_expiration_date(cls, products)
+        company_with_more_products = cls.get_company_more_products(cls, products)
+
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_fabrication_date}\n"
+            f"Data de validade mais próxima: {next_expiration_date}\n"
+            f"Empresa com mais produtos: {company_with_more_products}"
+        )
 
 
 test_produts = [
