9bc5ae8f3271c9d4ce4367124e7682a870b639df
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-13 19:38:14 -0300

    :sparkles: feat: adiciona boilerplate do complete report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -2,5 +2,10 @@ from simple_report import SimpleReport
 
 
 class CompleteReport(SimpleReport):
-    def most_products_stocket_by_company():
-        pass
+    def most_products_stocket_by_company(products):
+        return products
+
+    @classmethod
+    def generate(cls, products):
+        stock_by_company = cls.most_products_stocket_by_company(products)
+        return stock_by_company
