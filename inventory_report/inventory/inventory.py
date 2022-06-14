fffb575c63bb3e4c298ae5293411c45edf184c2b
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-13 21:22:54 -0300

    :recycle: refactor: refatora import_data para evitar complexidade de ifs

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,8 +2,9 @@ import csv
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
-class CSV_reader:
-    def read(path):
+
+class CSVReader:
+    def read(self, path):
         with open(path) as file:
             file_data = csv.DictReader(file, delimiter=",", quotechar='"')
             return list(file_data)
@@ -23,7 +24,11 @@ class Inventory:
         - "completo"
     """
     file_extension_from_path = path.split(".")[-1]
-    data = ''
-    if file_extension_from_path == "csv":
-      data = CSV_reader.read(path)
-    return cls.report_types[type].generate(data)
+  
+    file_reader = {
+      "csv": CSVReader.read(cls, path)
+    }
+  
+    products = file_reader[file_extension_from_path]
+  
+    return cls.report_types[type].generate(products)
