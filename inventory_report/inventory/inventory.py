4d10941adff02fa75fd5eceb6a234acdaec551e2
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-13 21:24:51 -0300

    :lipstick: style: identa código

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -11,24 +11,24 @@ class CSVReader:
 
 
 class Inventory:
-  report_types = {"simples": SimpleReport, "completo": CompleteReport}
+    report_types = {"simples": SimpleReport, "completo": CompleteReport}
 
-  @classmethod
-  def import_data(cls, path, type):
-    """
-      cls is the class
-      path is the path to the file
-      type is the type of data
-      there are 2 types of data:
-        - "simples"
-        - "completo"
-    """
-    file_extension_from_path = path.split(".")[-1]
-  
-    file_reader = {
-      "csv": CSVReader.read(cls, path)
-    }
-  
-    products = file_reader[file_extension_from_path]
-  
-    return cls.report_types[type].generate(products)
+    @classmethod
+    def import_data(cls, path, type):
+        """
+          cls is the class
+          path is the path to the file
+          type is the type of data
+          there are 2 types of data:
+            - "simples"
+            - "completo"
+        """
+        file_extension_from_path = path.split(".")[-1]
+
+        file_reader = {
+          "csv": CSVReader.read(cls, path)
+        }
+
+        products = file_reader[file_extension_from_path]
+
+        return cls.report_types[type].generate(products)
