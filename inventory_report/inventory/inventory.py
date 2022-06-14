5ed9a749d2d6915c1cf7a6bab816bc8d1fe987af
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-13 21:04:48 -0300

    :sparkles: feat: implementa método para ler CSV

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,17 @@
+import csv
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+class CSV_reader:
+    def read(path):
+        with open(path) as file:
+            file_data = csv.DictReader(file, delimiter=",", quotechar='"')
+            return list(file_data)
+
+
 class Inventory:
+  report_types = {"simples": SimpleReport, "completo": CompleteReport}
+
   @classmethod
   def import_data(cls, path, type):
     """
@@ -9,4 +22,8 @@ class Inventory:
         - "simples"
         - "completo"
     """
-    pass
+    file_extension_from_path = path.split(".")[-1]
+    data = ''
+    if file_extension_from_path == "csv":
+      data = CSV_reader.read(path)
+    return cls.report_types[type].generate(data)
