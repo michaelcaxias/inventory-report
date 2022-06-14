fe4c71520ca8b0d7d6959c5fe23838a556d73d5a
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-13 21:43:17 -0300

    :sparkles: feat: implementa método para leitor de json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -6,8 +7,15 @@ from inventory_report.reports.complete_report import CompleteReport
 class CSVReader:
     def read(self, path):
         with open(path) as file:
-            file_data = csv.DictReader(file, delimiter=",", quotechar='"')
-            return list(file_data)
+            products = csv.DictReader(file, delimiter=",", quotechar='"')
+            return list(products)
+
+
+class JSONReader:
+    def read(self, path):
+        with open(path) as file:
+            products = json.load(file)
+            return products
 
 
 class Inventory:
@@ -16,19 +24,23 @@ class Inventory:
     @classmethod
     def import_data(cls, path, type):
         """
-          cls is the class
-          path is the path to the file
-          type is the type of data
-          there are 2 types of data:
-            - "simples"
-            - "completo"
+        cls is the class
+        path is the path to the file
+        type is the type of data
+        there are 2 types of data:
+          - "simples"
+          - "completo"
         """
         file_extension_from_path = path.split(".")[-1]
 
         file_reader = {
-          "csv": CSVReader.read(cls, path)
+            "csv": CSVReader.read(cls, path),
+            "json": JSONReader.read(cls, path),
         }
 
         products = file_reader[file_extension_from_path]
 
         return cls.report_types[type].generate(products)
+
+
+test = Inventory.import_data("inventory_report/data/inventory.json", "simples")
