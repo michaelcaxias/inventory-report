405e1b65649d3b56e751da5cb6f8478e05b7b95c
Michael Caxias <79621661+michaelcaxias@users.noreply.github.com>
2022-06-16 18:06:30 -0300

    Update inventory.py

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -31,14 +31,6 @@ class Inventory:
 
     @classmethod
     def import_data(cls, path, type):
-        """
-        cls is the class
-        path is the path to the file
-        type is the type of data
-        there are 2 types of data:
-          - "simples"
-          - "completo"
-        """
         file_extension_from_path = path.split(".")[-1]
 
         products = ""
